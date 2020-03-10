5d6037d4d8d24598b44349070d0cbe0fb761540b
serrrgun <serrrgun@mail.ru>
2020-03-10 18:17:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    arr = expr.match(/.{1,10}/g);
+    
+    intArr = [];
+    
+    arr.forEach(element => {
+        if(element === '**********') {
+            intArr.push(' ')
+        } else {
+            
+            elem = element.match(/.{1,2}/g);
+
+            let int = []
+            
+            elem.forEach(element => {
+                if(element === '00') {
+                    int.push('')
+                } else if(element === '10') {
+                    int.push('.')
+                    console.log(int)
+                } else if(element === '11') {
+                    int.push('-')
+                }
+            })
+            
+            intArr.push(int.join('').trim())
+        }
+    });
+    
+    string = []
+    for(let i = 0; i < intArr.length; i++) {
+        if(intArr[i] === ' ') {
+            string.push(' ')
+        } else {
+            string.push(MORSE_TABLE[intArr[i]])
+        }
+        
+    }
+
+    return string.join('')
 }
 
 module.exports = {
